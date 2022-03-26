12c3b51896c49b8c93f35594e73182a1b9338ab3
Cirbugs <102011812+Cirbugs@users.noreply.github.com>
2022-03-26 17:24:43 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Cbtis119 Proyecto</title>
+        <link rel="icono" href="LogoCBTis119.png">
+        <meta name="description" content="Proyecto Cbtis119, Cristian Torres.">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="styles.css">
+        <ul class="menu">
+          <li><a href="#Info">Informacion</a></li>
+          <li><a href="#GYA">Gustos Y Aficiones</a></li>
+          <li><a href="#Estudios">Mis Estudios</a></li>
+        </ul>
+    </head>
+    <body bgcolor="#b6f1a7">
+        <h1 align="center">                <b>Cristian David Torres Cortina.</b> </h1>
+        <center>
+        <img src="LogoCBTis119.png" 
+        width="300" heigth="130 "alt="LogoCbtis"></center>
+          <hr color="black">
+        <center>
+        <font size=5 color="#d600d6"><h2 id="Info"> MI INFORMACION</h2></font>
+          <img src="gatito.jpg" align="left" width="170">
+          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, facilis? 
+            Obcaecati labore ut fugiat 
+            consectetur explicabo, exercitationem, aspernatur optio accusamus aut
+             voluptates quidem dolorem maiores asperiores nostrum modi odit vel!
+            <br>
+          </p> </center>
+          <br>
+          <h2 align="left"><b>Mis Datos Generales:</b></h2>
+          <div>
+          <ul>
+            <li>Nombre: <b>Lorem imps dolor sit amet</b>/li>
+            <li>Facebook:<a href="https://facebook.com/Cirbugs"><b>Cristian Torres</b></a></li>
+            <li>Telefono:<b>834######</b></li>
+            <li>Discord:<b>LifeBony#5318</b></li>
+            <li>Telegram:<a href="#">@Cirbugs</a></li>
+            <li>Correo:<a href="#">***@gmail.com</a></li>
+            </ul>
+            </div>
+            <hr color="black">
+            <h2 id="Estudios"><b>Mis Estudios:</b></h2>
+          <div>
+            <ol>
+              <li>Primaria:<b>Esc. Martha Jaramillo Hernandez</b></li>
+              <li>Secundaria:<b>Sec. Federalizada No. 1</b></li>
+              <li>Bachillerato:<b>Cbtis119</b></li>
+            </ol>
+          </div>
+          <hr color="black">
+          <h2 id="GYA"><b>Gustos y Aficiones</b></h2>
+          <p></p><strong>Mi Principal Aficion Es El <em>Bmx Deporte De Bicicleta</em></strong>
+          <a href="https://es.wikipedia.org/wiki/Ciclismo_BMX">Que Es Bmx?</a>
+          Tambien mis gustos de comida son <em>principalmente</em> los <strong>Mariscos</strong></p>
+          <hr color="black">
+          <br><br><br><br><br><br><br><br><br>
+          <center>
+          <p><b>Web By: </b> <a href="https://facebook/Cirbugs">Cirbugs</a></p>
+        </center>
+    </body>
+</html>
